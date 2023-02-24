4f16c22a524638501b18430ab66fdd45f19ca792
KosminaOlena <ok.art2017@gmail.com>
2023-02-24 15:50:51 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', markup);
 
-console.log(galleryItems);
+galleryContainer.addEventListener('click', onGalleryModal);
+
+function createGalleryMarkup(images) {
+    return images.map(({ preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+        </div>`
+    }).join('');
+};
+function onGalleryModal(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    event.preventDefault();
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`)
+    instance.show();
+};
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const markup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', markup);
 
+galleryContainer.addEventListener('click', onGalleryModal);
+
+function createGalleryMarkup(images) {
+    return images.map(({ preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+        `
+    }).join('');
+};
+
+function onGalleryModal(e) {
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    e.preventDefault();
+    let gallery = new SimpleLightbox('.gallery a');
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
+gallery.on('error.simplelightbox', function (e) {
+	console.log(e); // some usefull information
+});
+
+
+};
 console.log(galleryItems);
